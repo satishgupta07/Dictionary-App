49c031c2ff47dd812b120a7be07d2bc09f68d609
satishgupta07 <satishgupta.cse01@gmail.com>
2020-08-16 23:18:04 +0530

    fetch data from API
    Signed-off-by: satishgupta07 <satishgupta.cse01@gmail.com>

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let input = document.querySelector('#input');
 let searchBtn = document.querySelector('#search');
+let apiKey = '0cb565e4-f236-4f13-9818-576048f6df6e';
 
 searchBtn.addEventListener('click', function(e){
     e.preventDefault();
@@ -15,3 +16,13 @@ searchBtn.addEventListener('click', function(e){
 
     getData(word);
 })
+
+async function getData(word) {
+
+    // Ajax call
+    const response = await fetch(`https://www.dictionaryapi.com/api/v3/references/learners/json/${word}?key=${apiKey}`);
+    const data = await response.json();
+
+    console.log(data);
+
+}
