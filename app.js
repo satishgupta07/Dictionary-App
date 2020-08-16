ea118b35854e5aa27e0b3ce2e5256b3b3d8a5af1
satishgupta07 <satishgupta.cse01@gmail.com>
2020-08-16 23:35:58 +0530

    build the suggestion part
    Signed-off-by: satishgupta07 <satishgupta.cse01@gmail.com>

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let input = document.querySelector('#input');
 let searchBtn = document.querySelector('#search');
 let apiKey = '0cb565e4-f236-4f13-9818-576048f6df6e';
+let notFound = document.querySelector('.not__found');
 
 searchBtn.addEventListener('click', function(e){
     e.preventDefault();
@@ -23,6 +24,25 @@ async function getData(word) {
     const response = await fetch(`https://www.dictionaryapi.com/api/v3/references/learners/json/${word}?key=${apiKey}`);
     const data = await response.json();
 
-    console.log(data);
+      // if empty result 
+      if (!data.length) {
+        notFound.innerText = ' No result found';
+        return;
+      }
+
+       // If result is suggestions
+      if (typeof data[0] === 'string') {
+        let heading = document.createElement('h3');
+        heading.innerText = 'Did you mean?'
+        notFound.appendChild(heading);
+        data.forEach(element => {
+            let suggestion = document.createElement('span');
+            suggestion.classList.add('suggested');
+            suggestion.innerText = element;
+            notFound.appendChild(suggestion);
+            
+        })
+        return;
+    }
 
 }
